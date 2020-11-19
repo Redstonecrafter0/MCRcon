e97f4cd8dfc8146df69de8ae65955e97557fd060
Redstonecrafter0 <54239558+Redstonecrafter0@users.noreply.github.com>
2020-11-19 18:38:19 +0100

    made object oriented

--- demo.py
+++ demo.py
@@ -1,43 +1,35 @@
-from __future__ import print_function
 import argparse
-import socket
-import mcrcon
-
-
-# python 2 compatibility
-try:
-    input = raw_input
-except NameError:
-    pass
-
-
+from mcrcon import McRcon
+from getpass import getpass
 
 def main():
-    # Parse arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument("host")
+    parser.add_argument("host", type=str)
     parser.add_argument("port", type=int)
-    parser.add_argument("password")
+    parser.add_argument("password", type=str, required=False)
     args = parser.parse_args()
+    password = args.password
 
-    # Connect
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect((args.host, args.port))
+    rcon = McRcon(args.host, args.port)
 
     try:
-        # Log in
-        result = mcrcon.login(sock, args.password)
+        if not password:
+            password = getpass('Password: ')
+        result = rcon.login(password)
         if not result:
-            print("Incorrect rcon password")
+            print("Incorrect password")
             return
 
-        # Start looping
         while True:
-            request = input()
-            response = mcrcon.command(sock, request)
+            request = input('>')
+            if request in ('quit', 'exit'):
+                rcon.close()
+                print('Bye')
+                break
+            response = rcon.command(request)
             print(response)
     finally:
-        sock.close()
+        rcon.close()
 
 if __name__ == '__main__':
     main()
--- mcrcon.py
+++ mcrcon.py
@@ -1,90 +1,86 @@
-import collections
+import socket
 import struct
+import collections
 
-
-bufsize = 4096
-
-Packet = collections.namedtuple("Packet", ("ident", "kind", "payload"))
-
+BUFSIZE = 4096
+Packet = collections.namedtuple('Packet', ('ident', 'kind', 'payload'))
 
 class IncompletePacket(Exception):
     def __init__(self, minimum):
         self.minimum = minimum
 
+class McRcon:
+    def __init__(self, host:str='localhost', port:int=25575):
+        self._sock:socket.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._sock.connect((host, port))
 
-def decode_packet(data):
-    """
-    Decodes a packet from the beginning of the given byte string. Returns a
-    2-tuple, where the first element is a ``Packet`` instance and the second
-    element is a byte string containing any remaining data after the packet.
-    """
+    @staticmethod
+    def _decode_packet(data) -> Packet:
+        """
+        Decodes a packet from the beginning of the given byte string. Returns a
+        2-tuple, where the first element is a ``Packet`` instance and the second
+        element is a byte string containing any remaining data after the packet.
+        """
+        if len(data) < 14:
+            raise IncompletePacket(14)
 
-    if len(data) < 14:
-        raise IncompletePacket(14)
+        length = struct.unpack('<i', data[:4])[0] + 4
+        if len(data) < length:
+            raise IncompletePacket(length)
 
-    length = struct.unpack("<i", data[:4])[0] + 4
-    if len(data) < length:
-        raise IncompletePacket(length)
+        ident, kind = struct.unpack('<ii', data[4:12])
+        payload, padding = data[12:length-2], data[length-2:length]
+        assert padding == b'\x00\x00'
+        return Packet(ident, kind, payload)
 
-    ident, kind = struct.unpack("<ii", data[4:12])
-    payload, padding = data[12:length-2], data[length-2:length]
-    assert padding == b"\x00\x00"
-    return Packet(ident, kind, payload), data[length:]
+    @staticmethod
+    def _encode_packet(packet) -> bytes:
+        """
+        Encodes a packet from the given ``Packet` instance. Returns a byte string.
+        """
+        data = struct.pack('<ii', packet.ident, packet.kind) + packet.payload + b'\x00\x00'
+        return struct.pack('<i', len(data)) + data
 
+    def _receive_packet(self) -> Packet:
+        """
+        Receive a packet from the given socket. Returns a ``Packet`` instance.
+        """
+        data = b''
+        while True:
+            try:
+                return McRcon._decode_packet(data)
+            except IncompletePacket as e:
+                while len(data) < e.minimum:
+                    data += self._sock.recv(e.minimum - len(data))
 
-def encode_packet(packet):
-    """
-    Encodes a packet from the given ``Packet` instance. Returns a byte string.
-    """
+    def _send_packet(self, packet):
+        """
+        Send a packet to the given socket.
+        """
+        self._sock.sendall(McRcon._encode_packet(packet))
 
-    data = struct.pack("<ii", packet.ident, packet.kind) + packet.payload + b"\x00\x00"
-    return struct.pack("<i", len(data)) + data
+    def login(self, password) -> bool:
+        """
+        Send a "login" packet to the server. Returns a boolean indicating whether
+        the login was successful.
+        """
+        self._send_packet(Packet(0, 3, password.encode('utf8')))
+        packet = self._receive_packet()
+        return packet.ident == 0
 
+    def command(self, text) -> str:
+        """
+        Sends a "command" packet to the server. Returns the response as a string.
+        """
+        self._send_packet(Packet(0, 2, text.encode('utf8')))
+        self._send_packet(Packet(1, 0, b''))
+        response = b''
+        while True:
+            packet = self._receive_packet()
+            if packet.ident != 0:
+                break
+            response += packet.payload
+        return response.decode('utf8')
 
-def receive_packet(sock):
-    """
-    Receive a packet from the given socket. Returns a ``Packet`` instance.
-    """
-
-    data = b""
-    while True:
-        try:
-            return decode_packet(data)[0]
-        except IncompletePacket as exc:
-            while len(data) < exc.minimum:
-                data += sock.recv(exc.minimum - len(data))
-
-
-def send_packet(sock, packet):
-    """
-    Send a packet to the given socket.
-    """
-
-    sock.sendall(encode_packet(packet))
-
-
-def login(sock, password):
-    """
-    Send a "login" packet to the server. Returns a boolean indicating whether
-    the login was successful.
-    """
-
-    send_packet(sock, Packet(0, 3, password.encode("utf8")))
-    packet = receive_packet(sock)
-    return packet.ident == 0
-
-
-def command(sock, text):
-    """
-    Sends a "command" packet to the server. Returns the response as a string.
-    """
-
-    send_packet(sock, Packet(0, 2, text.encode("utf8")))
-    send_packet(sock, Packet(1, 0, b""))
-    response = b""
-    while True:
-        packet = receive_packet(sock)
-        if packet.ident != 0:
-            break
-        response += packet.payload
-    return response.decode("utf8")
+    def close(self):
+        self._sock.close()
